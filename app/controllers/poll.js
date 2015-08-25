edd277b72a6de6e703e1ef77971768950eec2a45
Andrew Richardson <andreric@cisco.com>
2015-08-25 15:09:56 +0000

    Add basic response hook

--- app/controllers/poll.js
+++ app/controllers/poll.js
@@ -24,6 +24,20 @@ function numberChoices(choices) {
   return output;
 }
 
+function errorResponse(response, msg, err) {
+  if (err) console.log(msg + '\n' + err);
+  else console.log(msg);
+  return response.render("error.ejs");
+}
+
+function getContextIOAccount(client, email, callback) {
+  client.accounts().get({email: email}, function(err, response) {
+    if (err || !response.body.length) return callback(err);
+    callback(null, response.body[0]['id']);
+  });
+
+}
+
 app.post('/', function (request, response) {
   response.render("preview.ejs", {
     email: request.body.email,
@@ -42,14 +56,11 @@ app.post('/send', function (request, response) {
   db.run("INSERT INTO question (owner, recipients, question, choices) VALUES (?, ?, ?, ?)",
     [from, recipients, question, choices],
     function (err) {
-      if (err) {
-        console.log("Error inserting into database");
-        console.log(err);
-        return response.render("error.ejs");
-      }
+      if (err) return errorResponse(response, "Error inserting into database", err);
 
       var id = this.lastID,
-        subject = "[Q" + id + "] " + question,
+        prefix = "[Q" + id + "]",
+        subject = prefix + " " + question,
         body = "You've been asked a question by " + from + ":\n" + question + "\n\n" +
           choices + "\n\nPlease respond to this email with a single number indicating your choice.";
 
@@ -70,17 +81,37 @@ app.post('/send', function (request, response) {
       };
 
       transporter.sendMail(mailOptions, function(err, info) {
-        if (err) {
-          console.log("Error sending email");
-          console.log(err);
-          return response.render("error.ejs");
-        }
+        if (err) return errorResponse(response, "Error sending email", err);
 
-        console.log('Message sent: ' + info.response);
-        return response.render("success.ejs");
+        var client = new contextio.Client({
+          key: CONTEXTIO_KEY,
+          secret: CONTEXTIO_SECRET
+        });
+
+        getContextIOAccount(client, EMAIL_USER, function(err, ctxID) {
+          if (err) return errorResponse(response, "Error connecting to Context.IO", err);
+
+          var urlPrefix = request.protocol + '://' + request.get('host') + '/poll',
+            hookOptions = {
+              filter_to: EMAIL_USER,
+              filter_subject: prefix,
+              callback_url: urlPrefix + '/response/' + id,
+              failure_notif_url: urlPrefix + '/failure/' + id
+            };
+
+          client.accounts(ctxID).webhooks().post(hookOptions, function(err) {
+            if (err) return errorResponse(response, "Error creating web hook", err);
+            return response.render("success.ejs");
+          });
+        });
       });
     }
   );
+});
+
+app.post('/response/:id', function (request, response) {
+  response.send();
+  console.log(response.body);
 });
 
 module.exports = app
