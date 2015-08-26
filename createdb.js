95fb044fca56588f649cfa0344c01e511b44a7da
Andrew Richardson <andreric@cisco.com>
2015-08-26 23:32:40 +0000

    Add UNIQUE constraint for usernames

--- createdb.js
+++ createdb.js
@@ -2,7 +2,7 @@
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('./db.sqlite3');
 
-db.run('CREATE TABLE user (id INTEGER PRIMARY KEY, username TEXT, password TEXT, confirmation_code TEXT)', function(err) {
+db.run('CREATE TABLE user (id INTEGER PRIMARY KEY, username TEXT, password TEXT, confirmation_code TEXT) UNIQUE (username)', function(err) {
   // do nothing
 });
 
