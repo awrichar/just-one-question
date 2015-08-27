39e20a3d0a265669ab4634e6382a3666b3e584ed
Andrew Richardson <andreric@cisco.com>
2015-08-27 15:11:21 +0000

    Log errors when creating db

--- createdb.js
+++ createdb.js
@@ -3,13 +3,13 @@ var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('./db.sqlite3');
 
 db.run('CREATE TABLE user (id INTEGER PRIMARY KEY, username TEXT, password TEXT, confirmation_code TEXT) UNIQUE (username)', function(err) {
-  // do nothing
+  if (err) console.log("Error creating user table: " + err);
 });
 
 db.run('CREATE TABLE question (id INTEGER PRIMARY KEY, user_id INTEGER, recipients TEXT, question TEXT)', function(err) {
-  // do nothing
+  if (err) console.log("Error creating question table: " + err);
 });
 
 db.run('CREATE TABLE response (question_id INTEGER, idx INTEGER, label TEXT, count INTEGER DEFAULT 0)', function(err) {
-  // do nothing
+  if (err) console.log("Error creating response table: " + err);
 });
