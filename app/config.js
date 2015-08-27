8abaf5458ca47774c710350efe7d143d44c8a9ac
Andrew Richardson <andreric@cisco.com>
2015-08-27 16:13:33 +0000

    Switch config to use environment vars

---
+++ app/config.js
@@ -0,0 +1,4 @@
+exports.CONTEXTIO_KEY = process.env.CONTEXTIO_KEY;
+exports.CONTEXTIO_SECRET = process.env.CONTEXTIO_SECRET;
+exports.EMAIL_USER = process.env.EMAIL_USER;
+exports.EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
--- app/config.sample.js
+++
@@ -1,4 +0,0 @@
-exports.CONTEXTIO_KEY = 'your_key';
-exports.CONTEXTIO_SECRET = 'your_secret';
-exports.EMAIL_USER = 'some.user@gmail.com';
-exports.EMAIL_PASSWORD = 'your_password';
